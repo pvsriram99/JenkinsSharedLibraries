18d98ef576273eef3c6c44ac70b16f2ad73bdada
pvsriram99 <84578341+pvsriram99@users.noreply.github.com>
2021-06-28 18:29:49 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -6,10 +6,10 @@ def call(String stageName){
      }
   else if ("${stageName}" == "SonarQube Report")
      {
-       sh "${mavenhome}/bin/mvn clean sonar:sonar"
+       sh "mvn clean sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
-       sh "${mavenhome}/bin/mvn clean deploy"
+       sh "mvn clean deploy"
      }
 }
